4f9563d3ab8cb0573881b8f115c3a85a1212a344
Emine Sarıgül <knemine92@gmail.com>
2023-03-22 19:15:59 +0300

    sonn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,66 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
 
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
 // Kodlar buraya gelecek!
+
+document.querySelector("h1").addEventListener("mouseover", function () {
+  this.style.backgroundColor = "lightblue";
+});
+
+document.querySelector("h1").addEventListener("mouseout", function () {
+  this.style.backgroundColor = "lightpink";
+});
+
+const nav = document.querySelector("nav");
+const link = nav.getElementsByTagName("a");
+nav.addEventListener("click", () => {
+  for (let i = 0; i < link.length; i++) {
+    link[i].style.backgroundColor = "lightblue";
+  }
+});
+
+const head = document.querySelector("header");
+document.addEventListener("scroll", () => {
+  head.style.backgroundColor = "lightblue";
+});
+
+const son = document.querySelector("h4");
+son.addEventListener("dblclick", () => {
+  son.style.backgroundColor = "lightpink";
+});
+
+const muzik = document.querySelector("h2");
+muzik.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href =
+    "https://www.youtube.com/watch?v=edxLzNVVVEM&ab_channel=DokuzSekizM%C3%BCzik";
+});
+
+const x = document.querySelectorAll(".btn");
+x.forEach((div) => {
+  div.addEventListener("mouseenter", () => {
+    div.style.backgroundColor = "lightblue";
+  });
+});
+
+x.forEach((div) => {
+  div.addEventListener("mouseleave", () => {
+    div.style.backgroundColor = "lightpink";
+  });
+});
+
+const imgHepsi = document.querySelectorAll("img");
+
+imgHepsi.forEach((img) => {
+  img.addEventListener("mouseover", () => {
+    img.style.transform = "scale(1.2)";
+  });
+  img.addEventListener("mouseout", () => {
+    img.style.transform = "scale(1)";
+  });
+});
